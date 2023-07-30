32432acd4761a61df5611d7840fa2814f385676b
azzumwaqar <azzumwaqar@gmail.com>
2023-07-30 18:54:29 +0100

    tests: successful data abstraction layer impl

--- src/test/java/com/qa/tests/LoginTests.java
+++ src/test/java/com/qa/tests/LoginTests.java
@@ -3,15 +3,35 @@ package com.qa.tests;
 import com.qa.BaseTest;
 import com.qa.pages.LoginPage;
 import com.qa.pages.ProductsPage;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.testng.Assert;
 import org.testng.annotations.*;
+import java.io.InputStream;
 
 public class LoginTests {
 
     private LoginPage loginPage;
+    private InputStream inputStream;
+    private JSONObject jsonObjLoginUsers;
 
     @BeforeClass
     public void beforeClass() throws Exception {
+        try {
+            String datafilename = "/data/loginUsers.json";
+            inputStream = getClass().getResourceAsStream(datafilename);
+            JSONTokener jsonTokener = new JSONTokener(inputStream);
+            jsonObjLoginUsers = new JSONObject(jsonTokener);
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if(inputStream!= null){
+                inputStream.close();
+            }
+        }
+
     }
 
     @AfterClass
@@ -32,9 +52,11 @@ public class LoginTests {
     @Test
     public void invalid_username_valid_password_unsuccessful_login() {
         //GIVEN - invalid username but correct password
+        JSONObject invalidUserObj = jsonObjLoginUsers.getJSONObject("invalidUser");
+
         loginPage
-                .enterUsername(LoginPage.incorrectUsername)
-                .enterPassword(LoginPage.password);
+                .enterUsername(invalidUserObj.getString("username"))
+                .enterPassword(invalidUserObj.getString("password"));
 
         //WHEN - login btn is pressed
         loginPage.pressLoginBtn();
@@ -46,9 +68,10 @@ public class LoginTests {
     @Test
     public void valid_username_invalid_password_unsuccessful_login() {
         //GIVEN - invalid username but correct password
+        JSONObject invalidUserObj = jsonObjLoginUsers.getJSONObject("invalidPassword");
         loginPage
-                .enterUsername(LoginPage.username)
-                .enterPassword(LoginPage.incorrectPassword);
+                .enterUsername(invalidUserObj.getString("username"))
+                .enterPassword(invalidUserObj.getString("password"));
 
         //WHEN - login btn is pressed
         loginPage.pressLoginBtn();
@@ -60,9 +83,11 @@ public class LoginTests {
     @Test
     public void valid_username_valid_password_successful_login() {
         //GIVEN - invalid username but correct password
+        JSONObject validUserObj = jsonObjLoginUsers.getJSONObject("validUser");
+
         loginPage
-                .enterUsername(LoginPage.username)
-                .enterPassword(LoginPage.password);
+                .enterUsername(validUserObj.getString("username"))
+                .enterPassword(validUserObj.getString("password"));
 
         //WHEN - login button is pressed
         ProductsPage productsPage = loginPage.pressLoginBtn();
