bcc31d1693a45cdbdd897da95fd58e0c5e7fce00
azzumwaqar <azzumwaqar@gmail.com>
2023-07-30 19:47:11 +0100

    chore: clean

--- src/test/java/com/qa/BaseTest.java
+++ src/test/java/com/qa/BaseTest.java
@@ -5,7 +5,6 @@ import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.io.InputStream;
 import java.time.Duration;
 import java.util.HashMap;
