0fe9b46d2f88904023d5bd874e139ed74a79a440
azzumwaqar <azzumwaqar@gmail.com>
2023-07-30 17:50:54 +0100

    tests: TestListener.java added

---
+++ src/test/java/com/qa/listeners/TestListener.java
@@ -0,0 +1,20 @@
+package com.qa.listeners;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class TestListener implements ITestListener {
+    @Override
+    public void onTestFailure(ITestResult result) {
+         if (result.getThrowable() != null){
+             StringWriter sw = new StringWriter();
+             PrintWriter printWriter = new PrintWriter(sw);
+             result.getThrowable().printStackTrace(printWriter);
+             System.out.println(sw);
+         }
+        ITestListener.super.onTestFailure(result);
+    }
+}
